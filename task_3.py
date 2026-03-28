23c80e553e28f529906d05f632d1ddf35d898867
robertIT1A <robertoguevarrajr25@gmail.com>
2026-03-28 21:20:46 +0800

    Add nested loops to print pattern of stars

---
+++ task_3.py
@@ -0,0 +1,8 @@
+for i in range (10):
+    for kalso in range (i):
+        print(" ", end=" ")
+    for j in range(i,10):
+        print("*", end=" ")
+    for l in range(i,10):
+        print("*", end=" ")
+    print()
